1a26d82846ffd97febf34751d7115d4d9a089db7
LeadKiller <hl2epicfan@gmail.com>
2017-06-25 11:38:12 -0700

    Updated toybox_main.lua in server
    added ent undo, thanks, hds46!

--- addon/lua/autorun/server/toybox_main.lua
+++ addon/lua/autorun/server/toybox_main.lua
@@ -18,12 +18,32 @@ net.Receive("ToyboxSpawnWep", function(len, ply)
 	ply:SelectWeapon(swep)
 end)
 
-net.Receive("ToyboxSpawnEnt", function(len, ply)
-	print("sweatg")
-	local ent = net.ReadString()
-	local sent = ents.Create(ent)
-	sent:SetPos(ply:GetEyeTrace().HitPos)
-	sent:Spawn()
+net.Receive("funboxSpawnEnt", function(len, ply)
+local ent = net.ReadString()
+local spawnfunc
+local vStart = ply:EyePos()
+local vForward = ply:GetAimVector()
+    local trace = {}
+trace.start = vStart
+trace.endpos = vStart + ( vForward * 4096 )
+trace.filter = ply
+
+tr = util.TraceLine( trace )
+local sent = scripted_ents.GetStored( EntityName )
+local SpawnFunction = scripted_ents.GetMember( ent, "SpawnFunction" )
+if ( !SpawnFunction ) then return end
+
+    spawnfunc = SpawnFunction( sent, ply, tr, ent )
+undo.Create( spawnfunc.PrintName )
+    undo.AddEntity( spawnfunc )
+    undo.SetPlayer( ply )
+    undo.SetCustomUndoText( "Undone " .. spawnfunc.PrintName)
+    undo.Finish()
+if ( IsValid( spawnfunc ) ) then
+spawnfunc:SetCreator( ply )
+end
+
+
 end)
 
 function ToyboxLoadDat(id)
